78c7d475383bff580981ad436939e84fbe58a1e8
declac <declan.mcgranahan@gmail.com>
2021-02-05 16:01:30 -0500

    Added Comments and Fixed the conversion fomula

--- Computer Systems/Assignment-1.c
+++ Computer Systems/Assignment-1.c
@@ -1,25 +1,25 @@
-#include <stdio.h>
+#include <stdio.h> 
 #include <stdlib.h> 
 
-double convert (double celsius) {
-   double fahrenheit = ((celsius * 5) / 9) + 32;
-   return fahrenheit;
+double convert (double celsius) { //Convert Function that takes a celcius input and converts it to fahrenheit
+   double fahrenheit = ((celsius * 9) / 5) + 32;
+   return fahrenheit; //Returns the fahrenheit number
 }
 
-int main(int argc, char **argv) {
-    double celsius;
+int main(int argc, char **argv) { //Main cunction that takes on command line input and returns a conversion
+    double celsius; //Defining the variables for the temperatures
     double fahrenheit;
-    if (argc < 2) {
-        printf("Please Enter At Least a Single Temperature for Conversion\n");
-        return 0;
+    if (argc < 2) { //Checks that there is at least one command line argument including the name of the file
+        printf("Please Enter At Least a Single Temperature for Conversion\n"); //IF this is not the case prints a frindly help message 
+        return 0; //And returns 0
     }
-    else if (argc > 2) {
-        printf("Please Enter Only a Single Temperature for Conversion\n");
-        return 0;
+    else if (argc > 2) { //Checks that there are no more than one command line argument including the name of the file
+        printf("Please Enter Only a Single Temperature for Conversion\n"); //IF this is not the case prints a frindly help message 
+        return 0; //And returns 0
     }
-    else {
-        celsius = strtod(argv[1], NULL);
-        fahrenheit = convert(celsius);
-        printf("%0.2lf Degrees Celsius Coverted to Fehrenheit is %0.2lf Degrees\n", celsius, fahrenheit);
+    else { //If there is only the proper amount of command line arguments the program converts 
+        celsius = strtod(argv[1], NULL); //Grabs the number in the command line and assigns it to the celsius variable 
+        fahrenheit = convert(celsius); //Runs the convert function defined above and assignns the result to the fahrenheit variable
+        printf("%0.2lf Degrees Celsius Coverted to Fehrenheit is %0.2lf Degrees\n", celsius, fahrenheit); //Prints out the Originally entered number allong with the new converted number
     }
 }
