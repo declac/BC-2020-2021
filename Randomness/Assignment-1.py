59064875ee375f2aa6e7fcb4bcf3eb22d703289a
declac <declan.mcgranahan@gmail.com>
2021-02-11 10:01:30 -0500

    Fixed PEP Conventions and added a main statement

--- Randomness/Assignment-1.py
+++ Randomness/Assignment-1.py
@@ -3,53 +3,60 @@ import numpy
 from decimal import *
 
 def most_probable(d, a, b) :
-    number_a = 0
-    number_b = 0
+    count_a = 0
+    count_b = 0
 
-    lim = 100*(6** (d));
-    print("Number of times rolling ", d, " dice is ", lim)
+    lim = 100*(6** (d))
+    print('Rolling %d dice %d times' % (d, lim))
 
     for i in range (0, lim) :
-        sum_roll = 0;
+        sum_roll = 0
         #print(i, "of ", lim)
 
         for j in range(1, d + 1) :
             roll = numpy.random.randint(1,7)
             #print("Die Roll = ", roll)
-            sum_roll += roll;
+            sum_roll += roll
             #print("Sum of Rolls: ", sum_roll, "After ", j, "rolls")
 
         if (sum_roll == a) :
             #print("Sum Roll is input a")
-            number_a += 1;
-            #print("Number of times sum = a is: ", number_a)
+            count_a += 1
+            #print("Number of times sum = a is: ", count_a)
         if (sum_roll == b) :
             #print("Sum roll is input a")
-            number_b += 1;
-            #print("Number of times sum = a is: ", number_b)
+            count_b += 1
+            #print("Number of times sum = a is: ", count_b)
    
 
-    p_a = float(Decimal(number_a)/Decimal(lim))
-    p_b = float(Decimal(number_b)/Decimal(lim))
-    print(p_a)
-    print(p_b)
-    z_score = 1.96
-    se_a = numpy.sqrt(p_a * (1-p_a) / lim)
-    lca = p_a - (z_score * se_a)
-    uca = p_a + (z_score * se_a)
-    print(lca, uca)
+    def confidence_interval(a, b, runs) :
+        z_score = 1.96
+        probability_a = float(Decimal(a)/Decimal(runs))
+        probability_b = float(Decimal(b)/Decimal(runs))
+        standard_error_a = numpy.sqrt(probability_a * (1-probability_a) / runs)
+        lca = probability_a - (z_score * standard_error_a)
+        uca = probability_a + (z_score * standard_error_a)
+        #print(lca, uca)
+        if (probability_b >= lca and probability_b <= uca) :
+            return 1
+        else: 
+            return 0
 
-    if (p_b >= lca and p_b <= uca) :
-        print(" Number of ", a, "s is: ", number_a, " and Number of ", b, "s is: ", number_b)
-        print(a, "and", b, "are Equally Likely");
-    elif (number_b > number_a) :
-        print(" Number of ", a, "s is: ", number_a, " and Number of ", b, "s is: ", number_b)
-        print(b, "is more likley")
+    if (confidence_interval(count_a, count_b, lim) == 1) :
+        print("Number of %d's is %d and Number of %d's is %d" % (a, count_a, b, count_b))
+        print("%d and %d are equally Likely!" % (a,b));
+    elif (count_b > count_a) :
+        print("Number of %d's is %d and Number of %d's is %d" % (a, count_a, b, count_b))
+        print("%d is more likley." % (b))
     else:
-        print(" Number of ", a, "s is: ", number_a, " and Number of ", b, "s is: ", number_b)
-        print(a, "is more likely")
+        print("Number of %d's is %d and Number of %d's is %d" % (a, count_a, b, count_b))
+        print("%d is more likely." % (a))
 
-d = int(input("Enter the number of dice: "));
-a = int(input("Enter a first sum: "))
-b = int(input("Enter a seccond sum: "))
-most_probable(d, a, b)
+def main() :
+    #d = int(input("Enter the number of dice: "));
+    #a = int(input("Enter a first sum: "))
+    #b = int(input("Enter a seccond sum: "))
+    most_probable(1, 3, 4)
+
+if (__name__ == "__main__") :
+    main()
