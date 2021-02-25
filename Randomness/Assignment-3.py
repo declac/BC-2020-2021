f3c369f53369126b0bff79d8b2b20d0c3b32665c
declac <declan.mcgranahan@gmail.com>
2021-02-25 17:06:16 -0500

    Changed to CDF

--- Randomness/Assignment-3.py
+++ Randomness/Assignment-3.py
@@ -4,14 +4,15 @@ from scipy.stats import binom
 trial = 0
 success = 0
 
+
 v = False
 
 while v != True:
-    if 0.05 > binom.pmf(success, trial, 0.5) > 0.03 and 0.05 > (binom.pmf(success-1, trial, 0.7)) > 0.03:
+    if 0.05 > (1-binom.cdf(success, trial, 0.5)) > 0.03 and 0.05 > (binom.cdf(success-1, trial, 0.7)) > 0.03:
         v = True
         break
     else:
         trial = random.randint(1,200)
         success = random.randint(1, trial)
 
-print(trial, success, binom.pmf(success, trial, 0.5), binom.pmf(success-1, trial, 0.5))
+print(trial, success, 1-binom.cdf(success, trial, 0.5), binom.cdf(success-1, trial, 0.7))
